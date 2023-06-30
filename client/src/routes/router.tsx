be7974607572d9dbcf41a9acafd7ccca924b435a
Кирилл <kirill.denisenko.01@gmail.com>
2023-06-30 13:39:08 +0300

    Updated router Added route for LogIn and SignUp

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,17 +1,19 @@
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-} from "react-router-dom";
-import SignUp from "../components/pages/SignUp";
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+import SignUp from '../components/pages/SignUp';
+import LogIn from '../components/pages/LogIn';
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-            <Route  path='/signup' element={<SignUp/>}/>
-            <Route path="/" element={<div>router</div>} />
-        </>
-    )
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<div>/</div>} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/login' element={<LogIn />} />
+    </>
+  )
 );
 
-export default router
+export default router;

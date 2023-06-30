38930d3df8bd1f57339a4ff6ebc8ad981880b9fb
Кирилл <kirill.denisenko.01@gmail.com>
2023-06-30 15:44:37 +0300

    Updated router for ResetPassword

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -5,11 +5,12 @@ import {
 } from 'react-router-dom';
 import SignUp from '../components/pages/SignUp';
 import LogIn from '../components/pages/LogIn';
+import { ResetPassword } from '../components/pages/ResetPassword';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<div>/</div>} />
+      <Route path='/reset-password' element={<ResetPassword />} />
       <Route path='/signup' element={<SignUp />} />
       <Route path='/login' element={<LogIn />} />
     </>

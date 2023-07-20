071e2bb93275c59766a7d5aeeac4689731711725
Кирилл <kirill.denisenko.01@gmail.com>
2023-07-21 00:00:05 +0300

    Updated router. Added basic Chat page

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -7,10 +7,14 @@ import SignUp from '../components/pages/SignUp';
 import LogIn from '../components/pages/LogIn';
 import { ResetPassword } from '../components/pages/ResetPassword';
 import { Verified } from '../components/pages/Verified';
-
+import ChatLayout from "../components/layouts/ChatLayout";
+import ChatOutlet from "../components/parts/ChatOutlet";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+        <Route path='/' element={<ChatLayout/>}>
+            <Route path={':id'} element={<ChatOutlet/>}/>
+        </Route>
       <Route path='/reset-password' element={<ResetPassword />} />
       <Route path='/signup' element={<SignUp />} />
       <Route path='/login' element={<LogIn />} />

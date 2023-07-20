6f83037442152ef6c43b87c1e5997307ebc23d16
Кирилл <kirill.denisenko.01@gmail.com>
2023-07-20 23:59:11 +0300

    Integrated AuthProvider in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,17 @@ import { Theme } from './hoc/Theme';
 import router from './routes/router';
 import { RouterProvider } from 'react-router-dom';
 import ToastProvider from './hoc/ToastifyProvider';
+import {AuthProvider} from "./hoc/AuthProvider";
 
 function App() {
   return (
-    <Theme>
-      <ToastProvider>
-        <RouterProvider router={router} />
-      </ToastProvider>
-    </Theme>
+      <AuthProvider>
+        <Theme>
+          <ToastProvider>
+            <RouterProvider router={router} />
+          </ToastProvider>
+        </Theme>
+      </AuthProvider>
   );
 }
 

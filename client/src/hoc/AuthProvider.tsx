ce7a155ef3bb9f6c16c9f3861f20717ad2d0a389
Кирилл <kirill.denisenko.01@gmail.com>
2023-07-21 16:04:08 +0300

    Fixed hook AuthProvider

--- client/src/hoc/AuthProvider.tsx
+++ client/src/hoc/AuthProvider.tsx
@@ -1,7 +1,6 @@
 import { createContext, useState } from 'react';
 import {DeleteItemInLocalStorage, GetItemInLocalStorage} from "../components/services/localStorage";
 import {LocalStorage} from "../enum/varibles";
-import {useNavigate} from "react-router-dom";
 
 export type TAuthContextInitial = {
     auth: boolean;
@@ -18,20 +17,17 @@ const initialContext: TAuthContextInitial = {
 export const AuthContext = createContext<TAuthContextInitial>(initialContext);
 
 export const AuthProvider = ({ children }: any) => {
-    const navigator = useNavigate()
     const [auth, setAuth] = useState<boolean>(
         !!GetItemInLocalStorage(LocalStorage.accessToken)
     );
 
     const login = () => {
         setAuth(true);
-        navigator('/')
     };
 
     const logout = () => {
         setAuth(false);
         DeleteItemInLocalStorage(LocalStorage.accessToken)
-        navigator('/login')
     };
 
     const value: TAuthContextInitial = { auth, login, logout };

ffa8a0282d86d6d40e22324f1f8781956fedd0d0
Кирилл <kirill.denisenko.01@gmail.com>
2023-08-02 12:05:55 +0300

    Updated router

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -8,18 +8,17 @@ import LogIn from '../components/pages/LogIn';
 import { ResetPassword } from '../components/pages/ResetPassword';
 import { Verified } from '../components/pages/Verified';
 import ChatLayout from "../components/layouts/ChatLayout";
-import ChatOutlet from "../components/parts/ChatOutlet";
+import ChatOutlet from "../components/pages/ChatOutlet";
 import PrivateRoute from "../hoc/PrivateRoute";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-        <Route path='/' element={<ChatLayout/>}>
-            <Route path={':id'} element={
-                <PrivateRoute>
-                    <ChatOutlet/>
-                </PrivateRoute>
-            }
-            />
+        <Route path='/' element={
+            <PrivateRoute>
+                <ChatLayout/>
+            </PrivateRoute>
+        }>
+            <Route path={':id'} element={<ChatOutlet/>} />
         </Route>
       <Route path='/reset-password' element={<ResetPassword />} />
       <Route path='/signup' element={<SignUp />} />

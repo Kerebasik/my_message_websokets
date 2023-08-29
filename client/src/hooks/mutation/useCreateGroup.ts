65594852aeaf90ad8181bf601c099a877dea1857
Кирилл <kirill.denisenko.01@gmail.com>
2023-08-29 13:05:34 +0300

    Created custom hook for create Group

---
+++ client/src/hooks/mutation/useCreateGroup.ts
@@ -0,0 +1,18 @@
+import {useMutation} from "@apollo/client";
+import {CREATE_GROUP} from "../../graphql/mutation/group";
+import {CreateGroupForm} from "../../components/pages/CreateGroup";
+
+
+const useCreateGroup = ()=>{
+    const [CreateGroup, {loading, error}] = useMutation(CREATE_GROUP)
+
+    const createGroup = async ({name, description}:CreateGroupForm)=>{
+        const data = await CreateGroup({variables:{group_name:name, description:description}})
+
+        return data
+    }
+
+    return {createGroup, loading, error}
+}
+
+export {useCreateGroup}
